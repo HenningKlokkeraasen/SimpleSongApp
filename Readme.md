53d70cf3bd928d5ce11f7b0ec21b83b30647564a
Henning Klokkeråsen <henning.klokkeraasen@gmail.com>
2022-12-03 00:21:02 +0100

    Version 0.1

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 WIP
 
 [Site](/site/index.html)
+
+* Data stored in JSON files
+* PowerShell scripts reads JSON files and outputs HTML files
+* Static site hosted in GitHub Pages
---
+++ src/temp.ps1
@@ -0,0 +1,10 @@
+$ht = @{}
+Get-ChildItem "..\data" | Foreach-Object {
+    $json = Get-Content $_.FullName | ConvertFrom-Json 
+    $heading = $json.heading
+    $htmlFileName = $_.Name.Replace(".json", ".html")
+    $ht.Add($htmlFileName, $htmlFileName)
+    "<!DOCTYPE html>`n<h1>$heading</h1>" | Out-File "..\site\$htmlFileName" -Force -Encoding utf8
+}
+$ht.keys | Foreach-Object {$index} {$index += "  <li><a href='/$_'>$_</a></li>`n"} {$index}
+"<!DOCTYPE html>`n<h1>Index</h1>`n<ul>`n$index</ul>" | Out-File "..\site\index.html" -Force -Encoding utf8
