f3a2695aafa4e2ea5997bfae52e8c23033f41bd2
Henning Klokkeråsen <henning.klokkeraasen@gmail.com>
2022-12-03 00:23:30 +0100

    Fix path

--- src/temp.ps1
+++ src/temp.ps1
@@ -6,5 +6,5 @@ Get-ChildItem "..\data" | Foreach-Object {
     $ht.Add($htmlFileName, $htmlFileName)
     "<!DOCTYPE html>`n<h1>$heading</h1>" | Out-File "..\site\$htmlFileName" -Force -Encoding utf8
 }
-$ht.keys | Foreach-Object {$index} {$index += "  <li><a href='/$_'>$_</a></li>`n"} {$index}
+$ht.keys | Foreach-Object {$index} {$index += "  <li><a href='$_'>$_</a></li>`n"} {$index}
 "<!DOCTYPE html>`n<h1>Index</h1>`n<ul>`n$index</ul>" | Out-File "..\site\index.html" -Force -Encoding utf8
